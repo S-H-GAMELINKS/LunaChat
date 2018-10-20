1c3c826ddf619f993c95aa3178cd0261175bdf4a
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-20 14:12:31 +0900

    using dotenv-webpack

--- assets/components/index.vue
+++ assets/components/index.vue
@@ -3,3 +3,12 @@
     <h1>Index Pages</h1>
 </div>    
 </template>
+
+<script>
+
+console.log(process.env.API_KEY);
+
+export default {
+    
+}
+</script>
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,11 +1,11 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: './index.js', // entry pointを起点にバンドルしていきます
-    output: { // 出力に関して
-      filename: 'index.js', // 出力するファイル名    
-      path: `${__dirname}/webpack/` // 出力するディレクトリ階層
-      // pathは絶対パスで指定、そのため __dirname でディレクトリ階層を取得しています
+    entry: './index.js', 
+    output: { 
+      filename: 'index.js',   
+      path: `${__dirname}/webpack/` 
     },
     module: {
         rules: [{
@@ -27,6 +27,7 @@ module.exports = {
     },
     plugins: [
         // ...
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new Dotenv()
       ]
   };
