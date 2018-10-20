7b74b909bdfb3ebf8019c85684f13b20b2b71bb3
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-20 15:25:58 +0900

    using dotenv-webpack & vuex

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue/dist/vue.esm';
-import Router from './router/router'
+import Router from './router/router';
+import Store from './store/store';
 import Header from './components/header.vue';
 import * as  Bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -8,6 +9,7 @@ Vue.use(Bootstrap)
 
 const hello = new Vue({
     router: Router,
+    store: Store,
     el: '#app',
     components: {
         'nav-bar': Header
